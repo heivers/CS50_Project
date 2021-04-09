d5b1c1f72f33b7e6c7b9e1f42af6f0a13b2b0715
Bart Heerkens <bartheerkens@Barts-MacBook-Pro.local>
2021-04-09 22:07:51 +0200

    added upload functionality, changed html files

--- application.py
+++ application.py
@@ -1,11 +1,23 @@
 """created by barthk12"""
-from flask import Flask, redirect, render_template, request, session
+import os
+from flask import Flask, flash, redirect, render_template, request, session, url_for, send_from_directory
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(),"static/photos")
+# create the upload folder should it not exist yet
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 
 # Configure app
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 # Reload templates automatically
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -24,15 +36,42 @@ app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_TYPE'] = "filesystem"
 Session(app)
 
+# Check if file should be allowed to upload
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/classifier")
+@app.route("/classifier", methods=['GET', 'POST'])
 def classifier():
+    if request.method == "POST":
+         # check if the post request has the file part
+         if "file" not in request.files:
+             flash("No file part")
+             return redirect((request.url))
+         file = request.files["file"]
+         if file.filename == "":
+             flash("No selected file")
+             return redirect(request.url)
+         if file and allowed_file(file.filename):
+             filename = secure_filename(file.filename)
+             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+             return render_template("result.html", filename=filename)
+         else:
+             flash("Filetype not supported")
+             return redirect(request.url)
+         
     return render_template("classifier.html")
 
 @app.route("/about")
 def about_me():
     return render_template("about.html")
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
